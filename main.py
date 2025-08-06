f52e8a106700190e251ce66e37841005d16d71f2
mgallardo6502 <marcelo.gallardo.6502@gmail.com>
2025-08-05 23:17:03 -0400

    Finished bookbot

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from stats import get_book_text
 from stats import word_count
 from stats import characters_count
+from stats import characters_sort
+from stats import sort_on
+book = "books/frankenstein.txt"
 
 def main():
-    with open("books/frankenstein.txt") as f:
+    with open(book) as f:
         book_as_string = get_book_text(f)
         amount_of_words = word_count(book_as_string)
         new_dictionary = characters_count(book_as_string)
-        print(amount_of_words, new_dictionary)
+        sorted_list = characters_sort(new_dictionary)
+        sorted_list.sort(reverse=True, key=sort_on)
+        
+        print("============ BOOKBOT ============")
+        print(f"Analysing book found at {book}...")
+        print("----------- Word Count ----------")
+        print(f"Found {amount_of_words} total words")
+        print("--------- Character Count -------")
+        for item in sorted_list:
+            if item["char"].isalpha():
+                print(f"{item["char"]}: {item["num"]}")
+        print("============= END ===============")
+        
 main()
--- stats.py
+++ stats.py
@@ -1,15 +1,25 @@
-def word_count(book):
-    return len(book.split())
-
 def get_book_text(book):
     return book.read()
 
+def word_count(book):
+    return len(book.split())
+
 def characters_count(book):
     dictionary = {}
-    for words in book.lower():
-        for character in words:
+    for words in book:
+        for character in words.lower():
             if character not in dictionary:
                 dictionary[character] = 1
             else:
                 dictionary[character] += 1
     return dictionary
+
+def characters_sort(list):
+    new_list = []
+    for character in list:
+        amount = list[character]
+        new_list.append({"char" : character, "num" : amount})
+    return new_list
+
+def sort_on(items):
+    return items["num"]
