7c4560a0f78ec4392a37b4786e0db754e4292c0e
mgallardo6502 <marcelo.gallardo.6502@gmail.com>
2025-08-05 23:29:19 -0400

    Updated bookbot to use sys.argv

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+import sys
 from stats import get_book_text
 from stats import word_count
 from stats import characters_count
 from stats import characters_sort
 from stats import sort_on
-book = "books/frankenstein.txt"
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def main():
-    with open(book) as f:
+    with open(sys.argv[1]) as f:
         book_as_string = get_book_text(f)
         amount_of_words = word_count(book_as_string)
         new_dictionary = characters_count(book_as_string)
@@ -14,7 +18,7 @@ def main():
         sorted_list.sort(reverse=True, key=sort_on)
         
         print("============ BOOKBOT ============")
-        print(f"Analysing book found at {book}...")
+        print(f"Analysing book found at {sys.argv[1]}...")
         print("----------- Word Count ----------")
         print(f"Found {amount_of_words} total words")
         print("--------- Character Count -------")
